d00835fb203be7387b4979f8882c2ea6e7778cdd
John Young <john.young@berkeley.edu>
2019-05-02 18:56:13 -0700

    updated folder names

--- proj1b/OffByOne.java
+++
@@ -1,9 +0,0 @@
-public class OffByOne implements CharacterComparator {
-    @Override
-    public boolean equalChars(char x, char y) {
-        if (x - y == 1 || x - y == -1) {
-            return true;
-        }
-        return false;
-    }
-}
